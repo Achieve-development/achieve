e27a92b9ccb4ab6fda388386ff632b296d05d40e
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-03-08 10:23:53 +0000

    :name_regist

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -31,7 +31,7 @@ class BlogsController < ApplicationController
 
     respond_to do |format|
       if @blog.save
-        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+        format.html { redirect_to @blog, notice: 'ブログが作成されました' }
         format.json { render :show, status: :created, location: @blog }
       else
         format.html { render :new }
@@ -47,7 +47,7 @@ class BlogsController < ApplicationController
     
     respond_to do |format|
       if @blog.update(blog_params)
-        format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
+        format.html { redirect_to @blog, notice: 'ブログが更新されました' }
         format.json { render :show, status: :ok, location: @blog }
       else
         format.html { render :edit }
@@ -63,7 +63,7 @@ class BlogsController < ApplicationController
     
     @blog.destroy
     respond_to do |format|
-      format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
+      format.html { redirect_to blogs_url, notice: 'ブログが削除されました' }
       format.json { head :no_content }
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
+  #validates :name, presence: true, length: { maximum: 50 }
 end
