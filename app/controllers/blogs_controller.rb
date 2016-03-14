933bead9644c3cbd2ab8086e898bd0b785013387
YujiMiyashita <ikkiiimonkey0229@gmail.com>
2016-03-14 06:06:36 +0000

    create_design

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -14,16 +14,19 @@ class BlogsController < ApplicationController
 
   # GET /blogs/new
   def new
-    @blog = Blog.new
+   redirect_to new_user_session_path unless user_signed_in?
+   @blog = Blog.new
   end
 
   # GET /blogs/1/edit
   def edit
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   # POST /blogs
   # POST /blogs.json
   def create
+    redirect_to new_user_session_path unless user_signed_in?
     @blog = Blog.new(blog_params)
 
     respond_to do |format|
@@ -40,6 +43,7 @@ class BlogsController < ApplicationController
   # PATCH/PUT /blogs/1
   # PATCH/PUT /blogs/1.json
   def update
+    redirect_to new_user_session_path unless user_signed_in?
     respond_to do |format|
       if @blog.update(blog_params)
         format.html { redirect_to @blog, notice: 'Blog was successfully updated.' }
@@ -54,6 +58,7 @@ class BlogsController < ApplicationController
   # DELETE /blogs/1
   # DELETE /blogs/1.json
   def destroy
+    redirect_to new_user_session_path unless user_signed_in?
     @blog.destroy
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: 'Blog was successfully destroyed.' }
@@ -66,6 +71,7 @@ class BlogsController < ApplicationController
     def set_blog
       @blog = Blog.find(params[:id])
     end
+    
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
